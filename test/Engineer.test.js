1e88e4d72a966d62bcb929facd6ecdc717ee3bd1
Zach Bryan <zacharybryan@icloud.com>
2020-09-15 05:07:51 -0700

    created getGitHub function, passed final Engineer test

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,12 +1,12 @@
 const Engineer = require("../lib/Engineer");
 
-test.only("Can set GitHUb account via constructor", () => {
+test("Can set GitHUb account via constructor", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.github).toBe(testValue);
 });
 
-test.only("getRole() should return \"Engineer\"", () => {
+test("getRole() should return \"Engineer\"", () => {
   const testValue = "Engineer";
   const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
   expect(e.getRole()).toBe(testValue);
