acd178a48cec7981ef640db6426a9e55362ff4be
Zach Bryan <zacharybryan@icloud.com>
2020-09-15 04:42:48 -0700

    added getRole function, passed 2nd manager test

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -7,7 +7,7 @@ test.only("Can set office number via constructor argument", () => {
   expect(e.officeNumber).toBe(testValue);
 });
 
-test('getRole() should return "Manager"', () => {
+test.only('getRole() should return "Manager"', () => {
   const testValue = "Manager";
   const e = new Manager("Foo", 1, "test@test.com", 100);
   expect(e.getRole()).toBe(testValue);
