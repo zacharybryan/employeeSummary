980609ab6a9ac489c17d63f78f20d79798339f47
Zach Bryan <zacharybryan@icloud.com>
2020-09-15 04:22:01 -0700

    added getRole function, passed final Employee test

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -56,8 +56,8 @@ test("Can get email via getEmail()", () => {
   expect(e.getEmail()).toBe(testValue);
 });
 
-// test("getRole() should return \"Employee\"", () => {
-//   const testValue = "Employee";
-//   const e = new Employee("Alice", 1, "test@test.com");
-//   expect(e.getRole()).toBe(testValue);
-// });
+test("getRole() should return \"Employee\"", () => {
+  const testValue = "Employee";
+  const e = new Employee("Alice", 1, "test@test.com");
+  expect(e.getRole()).toBe(testValue);
+});
